99252f0b4258c946b42571c87de40061e8db31da
Marc_Spectrum <sappakdee_t@silpakorn.edu>
2026-07-25 16:01:00 +0700

    Snook

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,24 @@ public class GameManager : MonoBehaviour
     private int playerScore;
     public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
 
+    [SerializeField]
+    private GameObject[] ballpositions;
+
+    [SerializeField]
+    private GameObject ballprefads;
+
     public static GameManager instance;
+
+    private void Start()
+    {
+        SetBall(Ballcolor.Red, 1);
+        SetBall(Ballcolor.Yellow, 2);
+        SetBall(Ballcolor.Green, 3);
+        SetBall(Ballcolor.Brown, 4);
+        SetBall(Ballcolor.Blue, 5);
+        SetBall(Ballcolor.Pink, 6);
+        SetBall(Ballcolor.Black, 7);
+    }
 
     void Awake()
     {
@@ -17,5 +34,14 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void SetBall(Ballcolor col, int i)
+    {
+        GameObject obj = Instantiate(ballprefads,
+                    ballpositions[i].transform.position,
+                    Quaternion.identity);
+        Srciptsball b = obj.GetComponent<Srciptsball>();
+        b.SetcolorAndPoint(col);
     }
 }
--- Assets/Scripts/Srcipts_ball.cs
+++ Assets/Scripts/Srcipts_ball.cs
@@ -20,6 +20,9 @@ public class Srciptsball : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private Ballcolor color;
 
+    [SerializeField]
+    private MeshRenderer rd;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
@@ -27,6 +30,10 @@ public class Srciptsball : MonoBehaviour, IPointerClickHandler
         Destroy(gameObject);
     }
 
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
     void Start()
     {
         
@@ -36,5 +43,46 @@ public class Srciptsball : MonoBehaviour, IPointerClickHandler
     void Update()
     {
         
+    }
+
+    public void SetcolorAndPoint(Ballcolor col)
+    {
+        switch(col)
+        {
+            case Ballcolor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case Ballcolor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case Ballcolor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case Ballcolor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case Ballcolor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case Ballcolor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case Ballcolor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+            case Ballcolor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+
+
+        }
     }
 }
